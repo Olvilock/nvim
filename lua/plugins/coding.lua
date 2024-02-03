2071c7b282ff55f69f7cc1f2ba9b8451e88c3cc7
Олексій Ігнатенко <oleksii.ihnatenko@gmail.com>
2024-02-03 23:56:28 +0200

    Remove <CR> default autocompletion

---
+++ lua/plugins/coding.lua
@@ -0,0 +1,14 @@
+return {
+  "hrsh7th/nvim-cmp",
+  ---@param opts cmp.ConfigSchema
+  opts = function(_, opts)
+    local cmp = require("cmp")
+    opts.mapping = vim.tbl_deep_extend("force", opts.mapping, {
+      ["<CR>"] = cmp.mapping.confirm({ select = false }),
+    })
+    -- opts.preselect = cmp.PreselectMode.None
+    opts.completion = {
+      completeopt = "menu,menuone,noinsert,noselect",
+    }
+  end,
+}

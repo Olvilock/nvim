c298d5901cf8292d849db9c550a3287590c2f3bc
Oleksii Ihnatenko <oleksii.ihnatenko@gmail.com>
2024-06-04 00:04:59 +0300

    Use default parsers in treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,20 +1,17 @@
 -- add more treesitter parsers
 return {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-        ensure_installed = {
-            "bash",
-            "html",
-            "css",
-            "json",
-            "lua",
-            "regex",
-            "vim",
-            "c",
-            "cpp",
-            "cuda",
-            "zig",
-            "rust",
-        },
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "html",
+      "css",
+      "json",
+      "regex",
+      "vim",
+      "cpp",
+      "cuda",
+      "zig",
+      "rust",
     },
+  },
 }
